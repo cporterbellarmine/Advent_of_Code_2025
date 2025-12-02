bde4d7c00b8b2f2684b126eeabf4a3acdf294c92
Christina <cporter@bellarmine.edu>
2025-12-01 21:56:13 -0500

    dial class outline

---
+++ Day1/Puzzle1/puzzle1solution/app/src/main/java/adventofcodeday1/Dial.java
@@ -0,0 +1,35 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Dial{
+
+    private Queue<String> history = new LinkedList<String>();
+
+    public Dial(){
+
+    }
+
+    private int moveLeft(int startNumber, int moveNumber){
+
+        return 0;
+    }
+
+    private int moveRight(int startNumber, int moveNumber){
+
+        return 0;
+
+    }
+
+    private void modifyHistory (String history){
+
+    }
+
+    private String getHistory(LinkedList<String> history){
+        return "";
+    }
+
+    private String getDial(){
+        return "";
+    }
+
+}
